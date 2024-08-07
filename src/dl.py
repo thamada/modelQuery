e1484faf0f6a62796c618c8c591f455f5c444a86
thamada <thamada@users.noreply.github.com>
2024-08-07 18:50:17 +0900

    update src/dl.py

--- src/dl.py
+++ src/dl.py
@@ -4,18 +4,21 @@ from huggingface_hub import snapshot_download
 
 token = os.getenv('HF_TOKEN')
 
-#path = snapshot_download(repo_id="meta-llama/Llama-2-7b", cache_dir="Llama-2-7b", use_auth_token=token)
-#path = snapshot_download(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", cache_dir="Llama3-8b", use_auth_token=token)
-#path = snapshot_download(repo_id="cyberagent/open-calm-small", cache_dir="cyberagent-small", use_auth_token=token)
-#path = snapshot_download(repo_id="cyberagent/calm3-22b-chat", cache_dir="cyberagent-calm3-22b-chat", use_auth_token=token)
-#path = snapshot_download(repo_id="meta-llama/Meta-Llama-3.1-70B-Instruct", cache_dir="Meta-Llama-3.1-70B-Instruct", use_auth_token=token)
-#path = snapshot_download(repo_id="meta-llama/Meta-Llama-3.1-405B-Instruct", cache_dir="Meta-Llama-3.1-405B-Instruct", use_auth_token=token)
-#path = snapshot_download(repo_id="meta-llama/Meta-Llama-3-8B", cache_dir="/mnt/models/Llama3-8b", use_auth_token=token)
-#path = snapshot_download(repo_id="meta-llama/Meta-Llama-3.1-8B", cache_dir="/mnt/models/Llama3.1-8b", use_auth_token=token)
+model = {'hf': 'openai-community/openai-gpt', 'dir': 'gpt-1'}
+model = {'hf': 'openai-community/gpt2-xl',    'dir': 'gpt2-xl'}
+model = {'hf': 'meta-llama/Llama-2-7b',       'dir': 'llama-2-7b'}
+model = {'hf': 'meta-llama/Llama-2-13b',      'dir': 'llama-2-13b'}
+model = {'hf': 'meta-llama/Llama-2-7b',       'dir': 'llama-2-7b'}
+model = {'hf': 'meta-llama/Meta-Llama-3-8B',              'dir': 'llama3-8b'}
+model = {'hf': 'meta-llama/Meta-Llama-3-8B-Instruct',     'dir': 'llama3-8b-inst'}
+model = {'hf': 'meta-llama/Meta-Llama-3-70B',             'dir': 'llama3-70b'}
+model = {'hf': 'meta-llama/Meta-Llama-3.1-8B',            'dir': 'llama3.1-8b'}
+model = {'hf': 'meta-llama/Meta-Llama-3.1-70B-Instruct',  'dir': 'llama-3.1-70b-inst'}
+model = {'hf': 'meta-llama/Meta-Llama-3.1-405B-Instruct', 'dir': 'llama-3.1-405b-inst'}
+model = {'hf': 'cyberagent/calm3-22b-chat',    'dir': 'cyberagent-calm3-22b-chat'}
+model = {'hf': 'cyberagent/open-calm-small',   'dir': 'cyberagent-small'}
 
-path = snapshot_download(repo_id="meta-llama/Meta-Llama-3.1-70B-Instruct", cache_dir="/mnt/models/Meta-Llama-3.1-70B-Instruct", use_auth_token=token)
-
-
+path = snapshot_download(repo_id=model['hf'], cache_dir=model['dir'], use_auth_token=token)
 
 print("_\n" * 20)
 print(path)
