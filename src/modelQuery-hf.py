433a465367291c33a5c8989424e15c51c4d46ace
Akira Tatsuno <tatsuno@azis-ai.com>
2024-08-22 10:08:08 +0900

    change torch.dtype from fp32 to bf16 and align output

--- src/modelQuery-hf.py
+++ src/modelQuery-hf.py
@@ -6,6 +6,7 @@ example:
 """
 
 import argparse
+import torch
 from transformers import AutoModelForCausalLM
 
 def get_n_params(_tensor_):
@@ -14,32 +15,53 @@ def get_n_params(_tensor_):
         n *= _tensor_.shape[i]
     return n
 
+def replace_key_names(key):
+    # Define your mapping here
+    replacements = {
+        "model.embed_tokens.weight": "tok_embeddings.weight",
+        "model.layers": "layers",
+        "self_attn.q_proj.weight": "attention.wq.weight",
+        "self_attn.k_proj.weight": "attention.wk.weight",
+        "self_attn.v_proj.weight": "attention.wv.weight",
+        "self_attn.o_proj.weight": "attention.wo.weight",
+        "mlp.gate_proj.weight": "feed_forward.w1.weight",
+        "mlp.up_proj.weight": "feed_forward.w3.weight",
+        "mlp.down_proj.weight": "feed_forward.w2.weight",
+        "input_layernorm.weight": "attention_norm.weight",
+        "post_attention_layernorm.weight": "ffn_norm.weight",
+        "model.norm.weight": "norm.weight",
+        "lm_head.weight": "output.weight"
+    }
+
+    # Replace the keys based on the mapping
+    for old_key, new_key in replacements.items():
+        key = key.replace(old_key, new_key)
+    return key
+
 def load_hf_model(model_path):
     # load HF model
-    hf_model = AutoModelForCausalLM.from_pretrained(model_path)
+    hf_model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16)
     m = hf_model.state_dict()
     print("")
-    print('-' * 80)
-    for key, value in hf_model.config.to_dict().items():
-        if key != "torch_dtype":
-            print(f"{key.rjust(35)}: {value}")
-    print('-' * 80)
+    print(hf_model.config)
     print("")
 
     total_params = 0
     total_bytes = 0
 
     for i, key in enumerate(m.keys()):
-
         n_param = get_n_params(m[key])
         n_bytes = n_param * m[key].element_size()
         total_params += n_param
         total_bytes += n_bytes
         s_size = str(m[key].size()).replace('torch.Size','').replace('(','').replace(')','')
 
+        # Apply key replacements
+        replaced_key = replace_key_names(key)
+
         print ('% 4d' % i,
                ': % 10d' % get_n_params(m[key]),
-               ': %050s' % key.ljust(50),
+               ': %035s' % replaced_key.ljust(35),
                ": %015s" % s_size.ljust(15),
                ": %s" % str(m[key].dtype)
                )
